de0b131741dd4133629ea7aa2dae5376c7190d96
mohlaroyivalijonova849 <mokhlaroyim.valijonova@gmail.com>
2023-07-17 20:01:52 +0500

    my first color

---
+++ app.js
@@ -0,0 +1,44 @@
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const colorText = document.querySelector(".color-text");
+const values = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+// random color function
+function getGradinet() {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.trunc(Math.random() * values.length);
+    color += values[randomNumber];
+  }
+  return color;
+}
+
+// set color to body
+function setGradinet() {
+  const color1 = getGradinet();
+  const color2 = getGradinet();
+  const randomDeg = Math.trunc(Math.random() * 360);
+  const bgColor = `linear-gradient(${randomDeg}deg, ${color1}, ${color2})`;
+  body.style.background = bgColor;
+  colorText.textContent = bgColor;
+}
+
+setGradinet();
+
+container.addEventListener("click", setGradinet);
